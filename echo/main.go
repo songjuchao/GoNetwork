907be3755eea1eb18d71059d5abb808f762fa9ac
songjuchao <songjuchao19960115@gmail.com>
2017-07-25 15:54:35 +0800

    add echo

---
+++ echo/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"net"
+	"log"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s hostname port\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	hostname := os.Args[1]
+	port := os.Args[2]
+	serverTcpAddr, err := net.ResolveTCPAddr("tcp", hostname+":"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tcpListener, err := net.ListenTCP("tcp", serverTcpAddr)
+	// NOTE: defer firstly or check err firstly
+	defer tcpListener.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		clientTcpConn, err := tcpListener.AcceptTCP()
+		// NOTE
+		defer clientTcpConn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go echo(clientTcpConn)
+	}
+}
+
+func echo(tcpConn *net.TCPConn) {
+	buffer := make([]byte, 4096)
+	for {
+		size, err := tcpConn.Read(buffer)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("read size %d", size)
+
+		size, err = tcpConn.Write(buffer[:size])
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("write size %d", size)
+	}
+}
